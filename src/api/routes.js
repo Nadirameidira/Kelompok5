41c28b231cac47250a177223f77ff7a42e00231f
Stesa Aurel Titania <stesaaureltitania@gmail.com>
2026-04-23 10:47:40 +0700

    Menambahkan route kenroute js

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const transactions = require('./components/transactions/transactions-route');
 const auth = require('./components/auth/auth-route');
+const products = require('./components/products/productManage-route');
 
 module.exports = () => {
   const app = express.Router();
@@ -9,5 +10,8 @@ module.exports = () => {
   // Mendaftarkan rute dari folder transactions
   transactions(app);
   auth(app);
+  products(app);
   return app;
 };
+
+// Mendaftarkan rute dari folder Product Management
